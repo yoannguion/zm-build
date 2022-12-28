8b78d9b70b760b57d0c12dcadb545cdcff6b7433
Yoann Guion <yguion@enovacom.fr>
2022-12-28 22:56:09 +0100

    custom packages and zimbra-package-stub

--- instructions/FOSS_remote_list.pl
+++ instructions/FOSS_remote_list.pl
@@ -1,4 +1,5 @@
 @ENTRIES = (
    "gh-zm" => { 'url-prefix' => "https://github.com/Zimbra", },
+   "gh-yg" => { 'url-prefix' => "https://github.com/yoannguion", },
    "gh-ks" => { 'url-prefix' => "https://github.com/kohlschutter", },
 );
