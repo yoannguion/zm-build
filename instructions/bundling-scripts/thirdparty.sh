8ffd61569cc633a4895bc556209591ee43059d26
Yoann Guion <yguion@enovacom.fr>
2022-12-29 12:21:29 +0100

    update zimbra and thirdparty package list

--- instructions/bundling-scripts/thirdparty.sh
+++ instructions/bundling-scripts/thirdparty.sh
@@ -30,7 +30,7 @@ CreateRhelPackage()
   echo "mkdir -p ${repoDir}/zm-build/${arch}"
   mkdir -p ${repoDir}/zm-build/${arch}
 
-  thirdparty=( aspell aspell-ca openssl libmilter libxml2 clamav apr apr-util libevent  heimdal curl  cyrus-sasl jetty-distribution httpd memcached openjdk perl-carp-clan perl-compress-raw-bzip2 perl-compress-raw-zlib perl-io-compress perl-exporter-tiny perl-list-moreutils  perl-digest-sha1 perl-html-parser perl-mail-dkim perl-net-dns perl-netaddr-ip perl-mail-spf perl-encode-detect perl-net-cidr-lite perl-mail-spamassassin perl-net-server php )
+  thirdparty=( aspell aspell-ca openssl libmilter libxml2 clamav apr apr-util libevent  heimdal curl  cyrus-sasl jetty-distribution httpd memcached openjdk perl-carp-clan perl-compress-raw-bzip2 perl-compress-raw-zlib perl-io-compress perl-exporter-tiny perl-list-moreutils  perl-digest-sha1  perl-digest-hmac perl-html-parser perl-crypt-openssl-rsa  perl-io-socket-inet6 perl-net-dns perl-mailtools perl-mail-dkim perl-netaddr-ip perl-error perl-net-dns-resolver-programmable perl-mail-spf perl-encode-detect perl-net-cidr-lite perl-mail-spamassassin perl-net-server php )
   for currentScript in ${thirdparty[@]}
   do
     echo "Building RPM ${currentScript} ..."
