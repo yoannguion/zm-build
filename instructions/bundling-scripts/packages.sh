c39a6b32678d2a82761c3999932cfe53943ab07a
Yoann Guion <yguion@enovacom.fr>
2022-12-30 10:47:29 +0100

    use packages build-order file

--- instructions/bundling-scripts/packages.sh
+++ instructions/bundling-scripts/packages.sh
@@ -30,20 +30,14 @@ CreateRhelPackage()
   echo "mkdir -p ${repoDir}/zm-build/${arch}"
   mkdir -p ${repoDir}/zm-build/${arch}
 
-
   while read p; do
-    echo "$p"
-  done <${repoDir}/packages/build-order
-
-  for currentScript in ${thirdparty[@]}
-  do
     echo "Building RPM ${p} ..."
     cd ${repoDir}/packages/${p}
     make all
     find ${repoDir}/packages/${p} -name "*.${arch}.rpm" -exec mv -v {} ${repoDir}/zm-build/${arch} \;
     cd ${repoDir}/zm-build/${arch} && createrepo  --update .
     sudo yum clean all --disablerepo=C6.10-base,C6.10-extras,C6.10-updates,epel
-  done
+done <${repoDir}/packages/build-order
 
 
 }
