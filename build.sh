846523df262286fb685213a7cbacb7758da55b6f
Yoann Guion <yguion@enovacom.fr>
2023-09-14 09:09:44 +0200

    10.0.4

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 #############
 # Variables #
 #############
-LIST_DEFAULT_TAG=9.0.0.p33,9.0.0.p32,9.0.0.p31,9.0.0.p30,9.0.0.p29,9.0.0.p28,9.0.0.p27,9.0.0.p26,9.0.0.p25,9.0.0.p24.1,9.0.0.p24,9.0.0.p23,9.0.0.p22,9.0.0.p21,9.0.0.p20,9.0.0.p19,9.0.0.p18,9.0.0.p17,9.0.0.p16,9.0.0.p15,9.0.0.p14,9.0.0.p13,9.0.0.p12,9.0.0.p11,9.0.0.p10,9.0.0.p9,9.0.0.p8,9.0.0.p7,9.0.0.p6.1,9.0.0.p6,9.0.0.p5,9.0.0.p4,9.0.0.p3,9.0.0.p2,9.0.0.p1,9.0.0
+LIST_DEFAULT_TAG=10.0.4,10.0.3,10.0.2,10.0.1,10.0.0
 PATCHNO=33
 
 #########################################
@@ -12,7 +12,7 @@ PATCHNO=33
 
 build_zimbra() {
   #ENV_CACHE_CLEAR_FLAG=true
-  $(dirname $0)/build.pl --ant-options -DskipTests=true --build-release=KEPLER --build-release-no=9.0.0 --build-release-candidate=ZZENO --build-type=FOSS --build-thirdparty-server=files.zimbra.com --git-default-tag=$LIST_DEFAULT_TAG --build-no=$PATCHNO`date +'%Y%m%d'`
+  $(dirname $0)/build.pl --ant-options -DskipTests=true --build-release=NIKOLATESLA --build-release-no=10.0.0 --no-interactive --build-release-candidate=ZZENO --build-type=FOSS --build-thirdparty-server=files.zimbra.com --git-default-tag=$LIST_DEFAULT_TAG --build-no=$PATCHNO`date +'%Y%m%d'`
 
   # Inform where archive can be found or error message if problem with build
   if [ $? == 0 ]
