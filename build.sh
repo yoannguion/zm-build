d036815bb14054c36c88cbd33b94fe42166ebb89
Yoann Guion <yoann.guion@gmail.com>
2024-02-04 19:42:14 +0100

    10.0.6

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 #############
 # Variables #
 #############
-LIST_DEFAULT_TAG=10.0.5,10.0.4,10.0.3,10.0.2,10.0.1,10.0.0-GA,10.0.0
+LIST_DEFAULT_TAG=10.0.6,10.0.5,10.0.4,10.0.3,10.0.2,10.0.1,10.0.0-GA,10.0.0
 PATCHNO=5
 
 #########################################
@@ -12,7 +12,7 @@ PATCHNO=5
 
 build_zimbra() {
   #ENV_CACHE_CLEAR_FLAG=true
-  $(dirname $0)/build.pl --ant-options -DskipTests=true --build-release=NIKOLATESLA --build-release-no=10.0.5 --no-interactive --build-release-candidate=ZZENO --build-type=FOSS --build-thirdparty-server=files.zimbra.com --git-default-tag=$LIST_DEFAULT_TAG --build-no=$PATCHNO`date +'%Y%m%d'`
+  $(dirname $0)/build.pl --ant-options -DskipTests=true --build-release=NIKOLATESLA --build-release-no=10.0.6 --no-interactive --build-release-candidate=ZZENO --build-type=FOSS --build-thirdparty-server=files.zimbra.com --git-default-tag=$LIST_DEFAULT_TAG --build-no=$PATCHNO`date +'%Y%m%d'`
 
   # Inform where archive can be found or error message if problem with build
   if [ $? == 0 ]
